143a1ff25859218ff5597ff0c388d812f56b3a96
Pablo_Araya <dsalattino@gmail.com>
2024-10-08 18:13:22 -0300

    soluciono problemas con el historial (no lo paso como parametro del main), reemplazo archivo aplicaciones.py por app.py

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-from aplicaciones import *
+from app import *
 from utils import vista_menu
 
 
-def main(historial=[]):
-    if (len(historial) > 0):
-        lista_historial = historial
-    else:
+def main():
+    n = 0
+    if n < 1:
         lista_historial = []
+
     while True:
         vista_menu()
         eleccion = input('Elige una opción (0-6): ')
         match eleccion:
             case '1':
                 calculadora_simple(lista_historial)
+                n += 1
             case '2':
                 sumatoria_promedio(lista_historial)
+                n += 1
             case'3':
                 area_circulo(lista_historial)
+                n += 1
             case '4':
                 mayor_de_tres(lista_historial)
+                n += 1
             case '5':
                 anio_bisiesto(lista_historial)
+                n += 1
             case '6':
-                if len(lista_historial) < 1:
+                if (len(lista_historial) < 1):
                     limpiar_consola()
                     print('Historial no disponible')
                     volver_menu()
